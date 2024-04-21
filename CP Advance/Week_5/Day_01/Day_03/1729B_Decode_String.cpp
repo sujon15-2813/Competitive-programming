ee69d1ff1e16b29925e5a9a1c04f3453e3f85713
Md Sujon Islam <86255046+sujon15-2813@users.noreply.github.com>
2024-04-21 23:45:03 +0600

    Add files via upload
    added week5day3 codes

---
+++ CP Advance/Week_5/Day_01/Day_03/1729B_Decode_String.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string ans = "";
+        for (int i = 0; i < n; i++)
+        {
+            if (i + 2 < n && s[i + 2] == '0' && (i + 3 >= n || s[i + 3] != '0'))
+            {
+                int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
+                ans.push_back((char)(96 + num));
+                i += 2;
+            }
+            else
+            {
+                int num = ((s[i] - '0'));
+                ans.push_back((char)(96 + num));
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
