79ec09460421a92ca66fd0acdf835a47a8d46a03
Md Sujon Islam <86255046+sujon15-2813@users.noreply.github.com>
2024-05-04 23:36:16 +0600

    Add files via upload
    added week7day2 codes

---
+++ CP Advance/Week_7/Day_01/Day_02/A_Divisible_Array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define str string
+#define ll long long
+#define by "nothing";
+#define pi pair<ll, ll>
+#define vec vector<ll>
+#define py cout << "YES\n"
+#define pn cout << "NO\n"
+#define pb(x) push_back(x)
+#define bee(x) x.begin(), x.end()
+#define loop(s, i, n) for (int i = s; i < n; i++)
+#define code ios::sync_with_stdio(false);
+#define mdsujonislam cin.tie(NULL);
+using namespace std;
+int main()
+{
+    code 
+    mdsujonislam
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+        {
+            cout << 2 * i << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
