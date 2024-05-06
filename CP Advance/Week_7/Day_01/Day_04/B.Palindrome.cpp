624078d84013ad0a091e0f866c0963c657da0288
Md Sujon Islam <86255046+sujon15-2813@users.noreply.github.com>
2024-05-06 22:22:15 +0600

    Add files via upload
    added week7day4 codes

---
+++ CP Advance/Week_7/Day_01/Day_04/B.Palindrome.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define str string
+#define ll long long
+#define by "nothing";
+#define pi pair<ll, ll>
+#define vec vector<ll>
+#define py cout << "YES\n"
+#define pn cout << "NO\n"
+#define pb(x) push_back(x)
+#define bee(x) x.begin(), x.end()
+#define loop(s, i, n) for (int i = s; i < n; i++)
+#define code ios::sync_with_stdio(false);
+#define mdsujonislam cin.tie(NULL);
+using namespace std;
+int main()
+{
+    code 
+    mdsujonislam
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        if (i & 2) 
+        {
+            cout << 'b';
+        }
+        else
+        {
+            cout << 'a'; 
+        }
+    }
+    cout << endl;
+    return 0;
+}
